6ef3c4e0a3765446ccd082d3b4c980cdfae0bf92
manulecam <lecam.manuel@gmail.com>
2023-01-11 19:04:02 -0300

    Map correction

--- js/index.js
+++ js/index.js
@@ -4,16 +4,21 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
-c.fillStyle = 'white';
+
 
 // We creates an image and we put the map
+c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 // MAP CREATION
 const image = new Image();
 image.src = './images/pelletTown.png';
 
+// PLAYER CREATION
+const playerImage = new Image();
+playerImage.src = './images/playerDown.png';
+console.log(playerImage)
 image.onload = () => {
-    c.drawImage(image, -750, -550);
+    c.drawImage(image, -750, -550)
+    c.drawImage(playerImage, 0, 0)
 }
-
